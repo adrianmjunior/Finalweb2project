9e8819b0ac4f658b363a078e0d40238a0e64c8a2
Adrian Martinez <adrjr01@hotmail.com>
2018-12-08 17:35:36 -0600

    JQuery
    Jquery added to logo class. now logo fades in and will fade out on button push.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+$('#btn-animate').on('click',function(){
+
+    $('#logo').addClass('animated fadeOutRightBig'); 
+    $('#btn-animate').addClass('animated fadeOutRightBig');
+})
+
+});
