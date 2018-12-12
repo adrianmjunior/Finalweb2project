c8f1c685baf4701c7ae27b519f38ab3a790fdb37
Adrian Martinez <adrjr01@hotmail.com>
2018-12-11 23:56:17 -0600

    added a bunch of stuff
    jquery function added to page. as well as button functoins

--- js/main.js
+++ js/main.js
@@ -4,6 +4,20 @@ $('#btn-animate').on('click',function(){
 
     $('#logo').addClass('animated fadeOutRightBig'); 
     $('#btn-animate').addClass('animated fadeOutRightBig');
+
 })
 
+$('#slidebutton').on('click',function(){
+
+    $('#pics').hide();
+
+})
+
+setTimeout(function(){
+    $('.navbar').fadeIn(200);
+},500);
 });
+
+
+
+
